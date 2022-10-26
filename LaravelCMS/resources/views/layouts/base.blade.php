d61eb86c223117d16b05befe36141d4d6f73d374
danilgryaznev <daniel58912007@gmail.com>
2022-10-26 22:46:15 +0300

    Пробуем laravel с нуля

--- LaravelCMS/resources/views/layouts/base.blade.php
+++ LaravelCMS/resources/views/layouts/base.blade.php
@@ -12,10 +12,13 @@
 <nav class="navbar navbar-light bg-light">
     <a href="{{ route('index') }}"
        class="navbar-brand mr-auto ">Главная</a>
+    @guest
     <a href="{{ route('register') }}"
        class="nav-item nav-link ">Регистрация</a>
     <a href="{{ route('login') }}"
        class="nav-item nav-link">Вход</a>
+    @endguest
+    @auth
     <a href="{{ route('home') }}"
        class="nav-item nav-link">Мои объявления</a>
     <form action="{{ route('logout') }}" method="POST"
@@ -23,6 +26,7 @@
         @csrf
         <input type="submit" class="btn btn-danger" value="Выход">
     </form>
+    @endauth
 </nav>
 <body>
     <div class="container">
--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -13,25 +13,31 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Тестовый контроллер объявлений
+Route::view('/','welcome');
 
-Route::get('/', [BbsController::class, 'index'])->name('index');
 
-Auth::routes();
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/home/add', [HomeController::class, 'showAddBbForm'])->name('bb.add');
-Route::post('home',[HomeController::class, 'storeBb'])->name('bb.store');
+//
+//Route::get('/', [BbsController::class, 'index'])->name('index');
+//
+//Auth::routes();
+//Route::get('/home', [HomeController::class, 'index'])->name('home');
+//Route::get('/home/add', [HomeController::class, 'showAddBbForm'])->name('bb.add');
+//Route::post('home',[HomeController::class, 'storeBb'])->name('bb.store');
+//
+//Route::get('home/{bb}/edit',[HomeController::class, 'showEditBbForm'])
+//    ->name('bb.edit')->middleware('can:update, bb');
+//Route::patch('home/{bb}', [HomeController::class, 'updateBb'])
+//    ->name('bb.update')->middleware('can:update, bb');
+//Route::get('home/{bb}/delete', [HomeController::class, 'showDeleteBbForm'])
+//    ->name('bb.delete')->middleware('can:destroy, bb');
+//Route::delete('home/{bb}/destroy', [HomeController::class, 'destroyBb'])
+//    ->name('bb.destroy')->middleware('can:destroy, bb');
+//
+//Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
+////Route::get('/phpmyinfo', function () {phpinfo();})->name('phpmyinfo');
 
-Route::get('home/{bb}/edit',[HomeController::class, 'showEditBbForm'])
-    ->name('bb.edit')->middleware('can:update, bb');
-Route::patch('home/{bb}', [HomeController::class, 'updateBb'])
-    ->name('bb.update')->middleware('can:update, bb');
-Route::get('home/{bb}/delete', [HomeController::class, 'showDeleteBbForm'])
-    ->name('bb.delete')->middleware('can:destroy, bb');
-Route::delete('home/{bb}/destroy', [HomeController::class, 'destroyBb'])
-    ->name('bb.destroy')->middleware('can:destroy, bb');
-
-Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
-//Route::get('/phpmyinfo', function () {phpinfo();})->name('phpmyinfo');
+// Пробуем создать контроллер управления пользователями
 
 
 
